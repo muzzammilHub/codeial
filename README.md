39dcf0c8926306409390d9716a40c114c3a9c75d
Muzzammil Hussain <095hussainmuzzammil@gmail.com>
2023-05-05 01:29:24 +0530

    add a controller and a router for home

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Scalable MVC(Model View Controller) Structure
+
+- Many actions in one file is called controller.
---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home= function(req, res){
+    return res.send('<h1>Home Controller</h1>')
+}
+
+// module.exports.actionName = function(req, res){} 
