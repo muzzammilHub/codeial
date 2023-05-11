db4ae613ddff0ee61aa771e096b04aa8d13fc2bb
Muzzammil Hussain <095hussainmuzzammil@gmail.com>
2023-05-11 14:42:23 +0530

    creating comment and render into views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -13,8 +13,16 @@ module.exports.home= function(req, res){
         //         console.log(err);
         //         return;
         //     })
-            Post.find({}).populate('user').exec()
+            Post.find({})
+                .populate('user')
+                .populate({
+                    path: 'comments',
+                populate: {
+                    path: 'user'
+                }})
+                .exec()
                 .then((posts)=>{
+                    
                     return res.render('home', {
                         title : 'Codeial | Home',
                         posts: posts
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const homeController = require('../controllers/home_controller')
 router.get('/', homeController.home);
 router.use('/users', require('./users'))
 router.use('/post', require('./post'));
-
+router.use('/comment', require('./comment'));
 
 // for any further routes access from here
 // router.use('/routeName', require('./routerfile'));
