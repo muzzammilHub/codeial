0359f5d69f3296f8fc90dc26d5ce4e77908d09df
Muzzammil Hussain <095hussainmuzzammil@gmail.com>
2023-05-12 15:27:51 +0530

    deleting comments from comment model and post model

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
+
+// creating comment
 module.exports.create = function(req, res){
     Post.findById(req.body.post)
         .then((post)=>{
@@ -25,3 +27,46 @@ module.exports.create = function(req, res){
             return;
         })
 }
+
+
+// deleting comment
+
+module.exports.destroy = function(req, res){
+        Comment.findById(req.params.id)
+            .then((comment)=>{
+                console.log(comment.user);
+                if(comment.user == req.user.id){
+
+                    let postId = comment.post;
+                    console.log(postId);
+                    
+                    comment.deleteOne({_id : req.params._id})
+                        .then((deleted)=>{
+                            if(!deleted){
+                                console.log("Error in deleting comment");
+                                return;
+                            }
+                            Post.findByIdAndUpdate(postId, { $pull: 
+                                { comments: {_id: req.params._id }} })
+                                .then((updatePost)=>{
+                                     if(!updatePost){
+                                         console.log("Not Updated Post");
+                                         return;
+                                    }
+                                    return res.redirect('back');
+                                })
+                                .catch((err)=>{
+                                    console.log(err);
+                                    return;
+                                })
+                        })
+                        .catch((err)=>{
+                            console.log(err);
+                            return;
+                        });
+                }
+                else
+                    return res.redirect('back');
+            })
+           
+    }
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){
    console.log(req.user);
@@ -8,6 +9,25 @@ module.exports.create = function(req, res){
     })
         .then((post)=>{
             return res.redirect('back');
+        })
+        .catch((err)=>{
+            console.log(err);
+            return;
+        });
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id)
+        .then((post)=>{
+            // .id means converting the object id into string
+            if(post.user == req.user.id){
+                post.deleteOne({_id: req.params._id});
+                Comment.deleteMany({post: req.params.id}).catch((err)=>{ console.log(err); return res.redirect('back');})
+                return res.redirect('back');
+            }
+            else{
+                return res.redirect('back');
+            }
         })
         .catch((err)=>{
             console.log(err);
